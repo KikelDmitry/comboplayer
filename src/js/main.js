6a8ede34930269fdc98ebfc2050b6f71d5bd2d9d
Kikel Dmitry <kikeldmitry@gmail.com>
2021-04-27 20:00:39 +0300

    setup in progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,47 @@
 //email validate
-{
-	let form = document.querySelector('.order-panel'),
-		input = form.querySelector('.order-panel__input'),
-		inputContainer = form.querySelector('.order-panel__email'),
-		check = form.querySelector('.order-panel__check'),
-		re = /\S+@\S+\.\S+/,
-		orderErr = form.querySelector('.order-panel__error');
+// {
+// 	let form = document.querySelector('.order-panel'),
+// 		input = form.querySelector('.order-panel__input'),
+// 		inputContainer = form.querySelector('.order-panel__email'),
+// 		check = form.querySelector('.order-panel__check'),
+// 		re = /\S+@\S+\.\S+/,
+// 		orderErr = form.querySelector('.order-panel__error'),
+// 		reset = form.querySelector('.order-panel__reset');
 
-	input.addEventListener('input', function () {
-		if (re.test(input.value)) {
-			inputContainer.classList.add('is-true')
-			inputContainer.classList.remove('is-false')
-			check.classList.add('is-true')
-			check.classList.remove('is-false')
-			orderErr.classList.remove('is-active')
-		} else {
-			inputContainer.classList.add('is-false')
-			inputContainer.classList.remove('is-true')
-			check.classList.add('is-false')
-			check.classList.remove('is-true')
-		}
-	})
-	input.addEventListener('blur', function () {
-		inputContainer.classList.remove('is-true', 'is-false')
-	})
-	form.addEventListener('submit', function(e) {
-		e.preventDefault();
-		if (check.classList.contains('is-true')) {
-			alert('submit success')
-		} else {
-			orderErr.classList.add('is-active')
-			setTimeout(function() {
-				orderErr.classList.remove('is-active')
-			}, 3000)
-		}
-	})
-}
+// 	input.addEventListener('input', function () {
+// 		if (re.test(input.value)) {
+// 			inputContainer.classList.add('is-true')
+// 			inputContainer.classList.remove('is-false')
+// 			check.classList.add('is-true')
+// 			check.classList.remove('is-false')
+// 			orderErr.classList.remove('is-active')
+// 		} else {
+// 			inputContainer.classList.add('is-false')
+// 			inputContainer.classList.remove('is-true')
+// 			check.classList.add('is-false')
+// 			check.classList.remove('is-true')
+// 		}
+// 	})
+// 	reset.addEventListener('click', function() {
+// 		input.value = '';
+// 		inputContainer.classList.remove('is-direct');
+// 		input.readOnly = false;
+// 	})
+// 	input.addEventListener('blur', function () {
+// 		inputContainer.classList.remove('is-true', 'is-false')
+// 	})
+// 	form.addEventListener('submit', function (e) {
+// 		e.preventDefault();
+// 		if (check.classList.contains('is-true')) {
+// 			alert('submit success')
+// 		} else {
+// 			orderErr.classList.add('is-active')
+// 			setTimeout(function () {
+// 				orderErr.classList.remove('is-active')
+// 			}, 3000)
+// 		}
+// 	})
+// }
 
 // order form
 {
@@ -57,5 +63,28 @@
 
 	inputs.forEach(function (input) {
 		input.addEventListener('change', getChecks)
+	})
+}
+{
+	// sticky panel fallback
+	$(window).scroll(function () {
+		let header = $('.header'),
+		 offset = header.height(),
+		sticky = $('.order-sticky');
+		if ($(window).scrollTop() > offset) {
+			header.css({
+				marginBottom: sticky.height()
+			});
+			sticky.css({
+				position: 'fixed'
+			});
+		} else {
+			header.css({
+				marginBottom: 0
+			});
+			sticky.css({
+				position: 'relative'
+			});
+		}
 	})
 }
